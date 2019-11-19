dc087ede7199c0deb8ee56a7bc57151d032222e0
DrHaid <40427630+DrHaid@users.noreply.github.com>
2019-11-19 09:18:56 +0100

    Performance Improvements
    - Changed Quality Settings
    - Locked Framerate to 60

---
+++ Assets/Scripts/FPSLimiter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSLimiter : MonoBehaviour
+{
+  public int maxFramerate;
+
+  void Awake()
+  {
+    QualitySettings.vSyncCount = 0;
+    Application.targetFrameRate = maxFramerate;
+  }
+}
