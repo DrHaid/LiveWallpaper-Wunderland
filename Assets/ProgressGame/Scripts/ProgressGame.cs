1bb8a321249719ea60c51890b6afd0fd573ef5b6
DrHaid <40427630+DrHaid@users.noreply.github.com>
2020-06-29 21:28:25 +0200

    Rudimentary progress bar, world border

---
+++ Assets/ProgressGame/Scripts/ProgressGame.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressGame : MonoBehaviour
+{
+  [Range(0, 1)]
+  public float Progress;
+
+  public GameObject UnlockableUIPrefab;
+  public List<Unlockable> Unlockables;
+
+  private Slider slider;
+
+  void Start()
+  {
+    slider = GetComponent<Slider>();
+    PositionUnlockables();
+  }
+
+  void Update()
+  {
+    slider.value = Progress;
+  }
+
+  private void PositionUnlockables()
+  {
+    var spacing = slider.fillRect.rect.width / Unlockables.Count;
+    for(int i = 0; i < Unlockables.Count; i++)
+    {
+      var leftAnchor = gameObject.transform.position;
+      leftAnchor.x -= slider.fillRect.rect.width / 2;
+      Unlockables[i].UiObject = Instantiate(
+        UnlockableUIPrefab,
+        leftAnchor + new Vector3(spacing * (i + 1), 0, 0), 
+        Quaternion.identity, 
+        gameObject.transform);
+      Unlockables[i].UiObject.GetComponent<Image>().sprite = Unlockables[i].Sprite;
+    }
+  }
+}
+
+[Serializable]
+public class Unlockable
+{
+  public bool Unlocked;
+  public Sprite Sprite;
+  public GameObject UiObject;
+}
---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+  public float dampTime = 0.15f;
+  public Transform target;
+
+  private Vector3 velocity = Vector3.zero;
+  private Transform cam;
+
+  private void Start()
+  {
+    cam = Camera.main.transform;
+  }
+
+  void LateUpdate()
+  {
+    if (target)
+    {
+      Vector3 destination = cam.transform.position;
+      destination.x = target.position.x;
+      transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+    }
+
+  }
+}
